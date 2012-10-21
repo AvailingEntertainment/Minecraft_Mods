dac6f5bb8f2aa6bdda888016862fa5fc1cecfe10
xKhronic <xkhronic@yahoo.com>
2012-10-21 13:49:01 -0700

    [8] Updates for tools
    Still not working... :/

--- src/placeHolder/mod_placeHolder.java
+++ src/placeHolder/mod_placeHolder.java
@@ -2,7 +2,7 @@
   * Copyright © 2012 AvailingEntertainment
   * 
   * File: mod_placeHolder.java
-  * Version: 0.2.1 (0214)
+  * Version: 0.2.3 (0237)
   * Developed and Maintained by AvailingEntertainment
   */
 package net.minecraft.src;
@@ -10,7 +10,7 @@ package net.minecraft.src;
 import java.util.Random;
 
 public class mod_placeHolder extends BaseMod {
-	public static final String Version = "0.2.1 (0214)";
+	public static final String Version = "0.2.3 (0237)";
 	public static final Block Platinum = new Block(165, 0, Material.rock).setBlockName("Platinum").setHardness(7F).setResistance(12.5F).setCreativeTab(CreativeTabs.tabBlock);	// Drops Ore
 	public static final Block Bisimuth = new BlockCustom(166, 0).setBlockName("Bisimuth").setHardness(5.5F).setResistance(10F).setCreativeTab(CreativeTabs.tabBlock);	// Drops Item
 	public static final Block Vranium = new Block(167, 0, Material.rock).setBlockName("Vranium").setHardness(4.8F).setResistance(2.0F).setCreativeTab(CreativeTabs.tabBlock);	// Drops Ore
@@ -30,33 +30,33 @@ public class mod_placeHolder extends BaseMod {
 	public static final Item ChromiumItem = new Item(1702).setItemName("ChromiumItem");
 	/** Item #1703, #1704, #1705 are reserved */
 	
-	public static final Achievement getPlatinum = new Achievement(1750, "getPlatinum", -9, -7,  Platinum, getBisimuth).registerAchievement();
+	public static final Achievement getVranium = new Achievement(1751, "getVranium", -5, -7, Vranium, null).registerAchievement();
 	public static final Achievement getBisimuth = new Achievement(1751, "getBisimuth", -7, -7, Bisimuth, getVranium).registerAchievement();
-	public static final Achievement getVranium = new Achievement(1752, "getVranium", -5, -7, Vranium, null).registerAchievement();
+	public static final Achievement getPlatinum = new Achievement(1752, "getPlatinum", -9, -7,  Platinum, getBisimuth).registerAchievement();
 	// public static final Achievement outGrowingNormal = new Achievement(1753, "outGrowingNormal", pos_x, pos_y, ???, getPlatinum).setSpecial().registerAchievement();
 	
-	public static final Item PlatinumPickaxe = new ItemCustomPickaxe(1706, EnumToolMaterialCustom.PLATINUM).setItemName("Platinum Pickaxe");
-	public static final Item BisimuthPickaxe = new ItemCustomPickaxe(1707, EnumToolMaterialCustom.BISIMUTH).setItemName("Bisimuth Pickaxe");
-	public static final Item CobaltPickaxe = new ItemCustomPickaxe(1708, EnumToolMaterialCustom.COBALT).setItemName("Cobalt Pickaxe");
-	public static final Item ChromiumPickaxe = new ItemCustomPickaxe(1709, EnumToolMaterialCustom.CHROMIUM).setItemName("Chromium Pickaxe");
+	public static final Item PlatinumPickaxe = new ItemCustomPickaxe(1706, EnumToolMaterialCustom.PLATINUM).setItemName("Platinum Pickaxe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item BisimuthPickaxe = new ItemCustomPickaxe(1707, EnumToolMaterialCustom.BISIMUTH).setItemName("Bisimuth Pickaxe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item CobaltPickaxe = new ItemCustomPickaxe(1708, EnumToolMaterialCustom.COBALT).setItemName("Cobalt Pickaxe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item ChromiumPickaxe = new ItemCustomPickaxe(1709, EnumToolMaterialCustom.CHROMIUM).setItemName("Chromium Pickaxe").setCreativeTab(CreativeTabs.tabTools);
 		
-	public static final Item PlatinumAxe = new ItemCustomAxe(1710, EnumToolMaterialCustom.PLATINUM).setItemName("Platinum Axe");
-	public static final Item BisimuthAxe = new ItemCustomAxe(1711, EnumToolMaterialCustom.BISIMUTH).setItemName("Bisimuth Axe");
-	public static final Item VraniumAxe = new ItemCustomAxe(1712, EnumToolMaterialCustom.VRANIUM).setItemName("Vranium Axe");
-	public static final Item ChromiumAxe = new ItemCustomAxe(1713, EnumToolMaterialCustom.CHROMIUM).setItemName("Chromium Axe");
+	public static final Item PlatinumAxe = new ItemCustomAxe(1710, EnumToolMaterialCustom.PLATINUM).setItemName("Platinum Axe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item BisimuthAxe = new ItemCustomAxe(1711, EnumToolMaterialCustom.BISIMUTH).setItemName("Bisimuth Axe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item VraniumAxe = new ItemCustomAxe(1712, EnumToolMaterialCustom.VRANIUM).setItemName("Vranium Axe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item ChromiumAxe = new ItemCustomAxe(1713, EnumToolMaterialCustom.CHROMIUM).setItemName("Chromium Axe").setCreativeTab(CreativeTabs.tabTools);
 	
-	public static final Item BisimuthSpade = new ItemCustomSpade(1714, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Axe");
-	public static final Item VraniumSpade = new ItemCustomSpade(1715, EnumToolMaterial.VRANIUM).setItemName("Vranium Axe");
-	public static final Item ThalliumSpade = new ItemCustomSpade(1716, EnumToolMaterial.THALLIUM).setItemName("Thallium Axe");
+	public static final Item BisimuthSpade = new ItemCustomSpade(1714, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Axe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item VraniumSpade = new ItemCustomSpade(1715, EnumToolMaterial.VRANIUM).setItemName("Vranium Axe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item ThalliumSpade = new ItemCustomSpade(1716, EnumToolMaterial.THALLIUM).setItemName("Thallium Axe").setCreativeTab(CreativeTabs.tabTools);
 	
-	public static final Item PlatinumSword = new ItemCustomSword(1717, EnumToolMaterial.PLATINUM).setItemName("Platinum Sword");
-	public static final Item BisimuthSword = new ItemCustomSword(1718, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Sword");
-	public static final Item VraniumSword = new ItemCustomSword(1719, EnumToolMaterial.VRANIUM).setItemName("Vranium Sword");
-	public static final Item CobaltSword = new ItemCustomSword(1720, EnumToolMaterial.COBALT).setItemName("Cobalt Sword");
+	public static final Item PlatinumSword = new ItemCustomSword(1717, EnumToolMaterial.PLATINUM).setItemName("Platinum Sword").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item BisimuthSword = new ItemCustomSword(1718, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Sword").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item VraniumSword = new ItemCustomSword(1719, EnumToolMaterial.VRANIUM).setItemName("Vranium Sword").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item CobaltSword = new ItemCustomSword(1720, EnumToolMaterial.COBALT).setItemName("Cobalt Sword").setCreativeTab(CreativeTabs.tabTools);
 	
-	public static final Item BisimuthHoe = new ItemCustomHoe(1721, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Hoe");
-	public static final Item ChromiumHoe = new ItemCustomHoe(1722, EnumToolMaterial.CHROMIUM).setItemName("Chromium Hoe");
-	public static final Item ThalliumHoe = new ItemCustomHoe(1723, EnumToolMaterial.THALLIUM).setItemName("Thallium Hoe");
+	public static final Item BisimuthHoe = new ItemCustomHoe(1721, EnumToolMaterial.BISIMUTH).setItemName("Bisimuth Hoe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item ChromiumHoe = new ItemCustomHoe(1722, EnumToolMaterial.CHROMIUM).setItemName("Chromium Hoe").setCreativeTab(CreativeTabs.tabTools);
+	public static final Item ThalliumHoe = new ItemCustomHoe(1723, EnumToolMaterial.THALLIUM).setItemName("Thallium Hoe").setCreativeTab(CreativeTabs.tabTools);
 	
 	public void load() {
 			/*-- Ore Register --*/
